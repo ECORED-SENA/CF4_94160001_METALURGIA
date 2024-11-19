a3148dd08cb72e8c5b59b0a04e873c63540bb4e8
Edwin Sneider Velandia Suarez <ivinernesto123@gmail.com>
2024-11-18 22:16:00 -0500

    avances

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -97,7 +97,7 @@
     #t_1_1.titulo-segundo.color-acento-contenido
       h2 1.1 Influencia de los elementos de aleación en el acero
     
-    .row.align-items-center
+    .row.align-items-center.mb-5
       .col-lg-8
         p.mb-4 Los aceros contienen elementos de aleación que mejoran sus características fundamentales. La influencia de estos elementos es variada, y al ajustar el porcentaje adecuado, se pueden obtener aceros con propiedades muy diferentes.
         p.mb-0 Es posible fabricar piezas de gran espesor con alta resistencia en su interior, herramientas resistentes a la corrosión, componentes mecánicos que combinan una gran dureza con tenacidad, y mecanismos que conservan su resistencia incluso a altas temperaturas. También se pueden crear moldes de formas complejas que no se deformen ni agrieten durante el tratamiento térmico.
@@ -105,7 +105,143 @@
         figure.mb-4.mb-lg-0
           img.mx-auto(src="@/assets/curso/temas/14.png",style="max-width: 400px;")
     
-    p(style="color:red;") Falta slyder aqui
+    .fondo-personalizado-1.f3.mb-5(data-aos="fade-up")
+      .fondo-personalizado-1__contenido.py-5
+        .bg-white.p-4.borde-20
+          SlyderA.slider-personalizado-1.pt-lg-3(tipo="b")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-5 Cobre (Cu)
+                p.mb-0 Aumenta la resistencia a la tracción, pero incrementa la fragilidad a altas temperaturas.
+              .col-lg-5
+                figure
+                  img.mx-auto(src="@/assets/curso/temas/17.png",style="max-width: 425px;")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-5 Cromo (Cr)
+                p.mb-0 Mejora la resistencia a la corrosión y oxidación, así como la resistencia a la abrasión y al desgaste. Facilita el tratamiento térmico y aumenta la resistencia mecánica a altas temperaturas. Es clave en la fabricación de aceros inoxidables.
+              .col-lg-5
+                figure
+                  img.mx-auto(src="@/assets/curso/temas/19.png",style="max-width: 425px;")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-5 Fósforo (P)
+                p.mb-0 Aumenta la resistencia y dureza en aceros bajos en carbono, mejora la resistencia a la corrosión y la maquinabilidad, pero reduce la tenacidad.
+              .col-lg-5
+                figure
+                  img.mx-auto(src="@/assets/curso/temas/20.png",style="max-width: 425px;")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-5 Hidrógeno
+                p.mb-0 Provoca fragilidad en el acero, siendo un elemento muy nocivo.
+              .col-lg-5
+                figure
+                  img.mx-auto(src="@/assets/curso/temas/21.png",style="max-width: 425px;")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-5 Aluminio (Al)
+                p.mb-0 Actúa como antioxidante y limita el crecimiento del grano, formando óxidos y nitruros. Es fundamental en ciertos aceros de nitruración.
+              .col-lg-5
+                figure
+                  img.mx-auto(src="@/assets/curso/temas/22.png",style="max-width: 425px;")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-5 Azufre (S)
+                p.mb-0 Aumenta la fragilidad, pero facilita el mecanizado.
+              .col-lg-5
+                figure
+                  img.mx-auto(src="@/assets/curso/temas/23.png",style="max-width: 425px;")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-5 Carbono
+                p.mb-0 Incrementa la resistencia, el límite de elasticidad y la dureza, aunque disminuye la ductilidad y maleabilidad.
+              .col-lg-5
+                figure
+                  img.mx-auto(src="@/assets/curso/temas/24.png",style="max-width: 425px;")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-5 Manganeso (Mn)
+                p.mb-0 Elimina la fragilidad en caliente causada por el azufre y mejora la resistencia al desgaste y a la oxidación. Facilita el tratamiento térmico.
+              .col-lg-5
+                figure
+                  img.mx-auto(src="@/assets/curso/temas/25.png",style="max-width: 425px;")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-5 Molibdeno (Mo)
+                p.mb-0 Mejora el tratamiento térmico, la resistencia mecánica a altas temperaturas y la resistencia al desgaste.
+              .col-lg-5
+                figure
+                  img.mx-auto(src="@/assets/curso/temas/26.png",style="max-width: 425px;")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-5 Níquel (Ni)
+                p.mb-0 Aumenta la resistencia en aceros no templados, proporciona tenacidad a bajas temperaturas y mejora la resistencia a la corrosión.
+              .col-lg-5
+                figure
+                  img.mx-auto(src="@/assets/curso/temas/27.png",style="max-width: 425px;")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-5 Nitrógeno (N)
+                p.mb-0 Facilita el tratamiento térmico y aumenta la resistencia de algunos aceros inoxidables.
+              .col-lg-5
+                figure
+                  img.mx-auto(src="@/assets/curso/temas/28.png",style="max-width: 425px;")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-5 Oxígeno (O)
+                p.mb-0 Incrementa la fragilidad del acero y disminuye su resistencia a los impactos.
+              .col-lg-5
+                figure
+                  img.mx-auto(src="@/assets/curso/temas/29.png",style="max-width: 425px;")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-5 Plomo (Pb)
+                p.mb-0 Facilita el mecanizado.
+              .col-lg-5
+                figure
+                  img.mx-auto(src="@/assets/curso/temas/30.png",style="max-width: 425px;")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-5 Silicio (Si)
+                p.mb-0 Actúa como desoxidante y aumenta la resistencia de los aceros bajos en carbono. Se usa en la construcción de chapas magnéticas.
+              .col-lg-5
+                figure
+                  img.mx-auto(src="@/assets/curso/temas/31.png",style="max-width: 425px;")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-5 Titanio (Ti)
+                p.mb-0 Actúa como desoxidante y mejora la capacidad de conformado del material.
+              .col-lg-5
+                figure
+                  img.mx-auto(src="@/assets/curso/temas/32.png",style="max-width: 425px;")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-5 Boro (B)
+                p.mb-0 Facilita el tratamiento térmico y aumenta la dureza en aceros inoxidables.
+              .col-lg-5
+                figure
+                  img.mx-auto(src="@/assets/curso/temas/33.png",style="max-width: 425px;")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-5 Vanadio (V)
+                p.mb-0 Facilita el tratamiento térmico, mejora la resistencia al desgaste en herramientas y refuerza aceros de bajo carbono.
+              .col-lg-5
+                figure
+                  img.mx-auto(src="@/assets/curso/temas/34.png",style="max-width: 425px;")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-5 Wolframio (W)
+                p.mb-0 Aumenta la resistencia al desgaste en herramientas y facilita el tratamiento térmico.
+              .col-lg-5
+                figure
+                  img.mx-auto(src="@/assets/curso/temas/35.png",style="max-width: 425px;")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-5 Cobalto (Co)
+                p.mb-0 Mejora la resistencia a la corrosión y la abrasión, y aumenta la dureza a altas temperaturas.
+              .col-lg-5
+                figure
+                  img.mx-auto(src="@/assets/curso/temas/36.png",style="max-width: 425px;")
 
     Separador
 
@@ -143,7 +279,64 @@
       .cont-text 
         h3 Propiedades
 
-    p.mb-4 El punto de fusión de las fundiciones varía entre 1100 y 1450 °C, según su tipo, y su peso específico oscila entre 6.7 y 6.8 gramos por centímetro cúbico.
+    p.mb-0 El punto de fusión de las fundiciones varía entre 1100 y 1450 °C, según su tipo, y su peso específico oscila entre 6.7 y 6.8 gramos por centímetro cúbico.
+
+    p(style="color:red;") Tabs aqui
+
+    .titulo-decorativo.mq-p-1.my-5
+      .cont-figura
+        img(src="@/assets/curso/temas/img-tit-dec.svg")
+      .cont-text 
+        h3 La conservación del medio ambiente en la siderurgia
+    
+    .row.align-items-center.mb-5
+      .col-md-10
+        p.mb-0 La conservación del medio ambiente es una prioridad para las empresas, que buscan un uso racional de los recursos naturales y la protección de las personas. Sin embargo, en el caso de la industria siderúrgica, los desafíos ambientales son significativos:
+      .col-md-2.order-first.order-md-last
+        figure.mb-4.mb-md-0
+          img.mx-auto(src="@/assets/curso/temas/37.svg",style="max-width: 93px;")
+    
+    .row.justify-content-center
+      .col-lg-5.mb-4
+        .fondo-color-sistema-m.h-100.borde-17
+          .position-relative.d-flex.justify-content-center.mb-5
+            figure.mb-4.mb-lg-0
+              img.mx-auto(src="@/assets/curso/temas/38.png")
+            .texto-p-1.px-3
+              h4.mb-0.p-1 Emisiones de CO₂
+
+          p.p-4 Las empresas siderúrgicas son responsables de aproximadamente el 27 % de las emisiones de CO₂ a nivel mundial. Sin embargo, la contaminación generada por los hornos de arco eléctrico es menor en comparación con otros métodos.
+      .col-lg-5.mb-4
+        .fondo-color-sistema-m.h-100.borde-17
+          .position-relative.d-flex.justify-content-center.mb-5
+            figure.mb-4.mb-lg-0
+              img.mx-auto(src="@/assets/curso/temas/39.png")
+            .texto-p-1.px-3
+              h4.mb-0.p-1 Tipos de siderurgia y su impacto
+
+          p.p-4.pb-3 #[b Siderurgia integral]. Cubre todo el proceso, desde la extracción de minerales, lo que genera una mayor cantidad de emisiones y residuos.
+          p.p-4.pt-0 #[b Siderurgia no integral]. Utiliza hornos eléctricos y chatarra como materia prima, lo que reduce las emisiones de CO₂.
+
+    p.my-4 Los desafíos ambientales en la siderurgia son: 
+
+    .row.align-items-center
+      .col-lg-3
+        figure.mb-4.mb-lg-0
+          img.mx-auto(src="@/assets/curso/temas/40.png",style="max-width: 296px;")
+      .col-lg-9
+        AcordionA.acordion-personalizado-1.mb-0(tipo="a" clase-tarjeta="tarjeta")
+          .div(titulo="Uso del agua")
+            .ps-md-5.ms-md-3
+              p Las instalaciones de colada continua requieren grandes cantidades de agua para la refrigeración, lo que puede resultar en aguas residuales con alto contenido de aceites, generando problemas de contaminación hídrica. Es fundamental el reciclaje de más del 90 % del agua utilizada y la depuración de las aguas residuales restantes.
+          .div(titulo="Generación de escorias y residuos")
+            .ps-md-5.ms-md-3
+              p Las escorias producidas durante los procesos metalúrgicos, si no se manejan adecuadamente, pueden contaminar el suelo y el agua. En las plantas de fundición, se generan residuos como arena usada, restos de moldes y escorias, lo que requiere un manejo eficiente para reducir su impacto ambiental.
+          .div(titulo="Captación de polvo")
+            .ps-md-5.ms-md-3
+              p La instalación de sistemas de captación de polvo con filtros de mangas es una solución eficaz para minimizar la emisión de partículas contaminantes en la atmósfera.
+
+
+
 
 </template>
 
